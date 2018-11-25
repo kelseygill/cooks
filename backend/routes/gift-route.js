e3ae3c7f2b8d23aa557926c06063ebf443bb98c3
Will Wise <william.wise@siteminder.com>
2018-11-25 13:18:24 +1100

    Before my shit dies

--- backend/routes/gift-route.js
+++ backend/routes/gift-route.js
@@ -6,13 +6,19 @@ module.exports = function (request, response) {
   const domain = 'sandbox032e3089bedb4b49ab3675a1b1f66763.mailgun.org';
   const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
-  const toBrokenLines = 
+  const toBrokenLines = (obj) => {
+    let body = []
+    for (let prop in obj) {
+      body.push(`${prop}: ${obj[prop]}`)
+    }
+    return body.join('\n')
+  }
 
   const data = {
     from: 'The Cooks Garden <me@samples.mailgun.org>',
     to: 'kelsey.gill.work@live.com.au',
     subject: 'A new gift request',
-    text: request.body.
+    text: toBrokenLines(request.body)
   };
 
   mailgun.messages().send(data, function (error, body) {
